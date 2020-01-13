1b8174517c1badab221393f698f8c649d5245004
wudongbj <wudongbj@fenbi.com>
2020-01-13 19:27:24 +0800

    feat:init project

---
+++ index.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+
+const fs = require('fs') 
+const { promisify } = require('util')
+const program = require('commander')
+const pacote = require('pacote')
+const packageConfig = require('./package')
+
+// const pkgFile = fs.readFileSync(file)
